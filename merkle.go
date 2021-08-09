9880a3762c4bfa03079e7e224716e39cb85ea926
Steven Kao <st.kao.05@gmail.com>
2021-08-09 16:55:01 +0800

    Refactoring

--- merkle.go
+++ merkle.go
@@ -5,6 +5,7 @@ import (
 	"math"
 	"sort"
 	"time"
+
 	"github.com/spaolacci/murmur3"
 )
 
@@ -15,29 +16,6 @@ type Trie struct {
 
 var Hasher hash.Hash32 = murmur3.New32WithSeed(0)
 var SlotMilli int64 = 60 * 1000
-
-func numToDigits(num int64) []int64 {
-	digitCount := int(math.Log10(float64(num))) + 1
-	res := make([]int64, 0, digitCount)
-
-	for i := digitCount - 1; i >= 0; i-- {
-		digit := int64(math.Mod(float64(num)/math.Pow(10, float64(i)), 10))
-		res = append(res, digit)
-	}
-
-	return res
-}
-
-func digitsToNum(digits []uint8) int64 {
-	var res = int64(0)
-	var n = len(digits)
-
-	for i := 0; i < n; i++ {
-		res += int64(digits[i]) * int64(math.Pow10(n-i-1))
-	}
-
-	return res
-}
 
 func Insert(trie *Trie, id string, timestamp time.Time) {
 	defer Hasher.Reset()
@@ -73,6 +51,79 @@ func Insert(trie *Trie, id string, timestamp time.Time) {
 
 		node = child
 	}
+}
+
+func BranchPoint(trie1 *Trie, trie2 *Trie) time.Time {
+	node1 := trie1
+	node2 := trie2
+	digits := []uint8{}
+
+loop:
+	for {
+		switch {
+		case node1 == nil && node2 == nil:
+			break loop
+		case node1 != nil && node2 == nil:
+			digits = append(digits, minTime(node1)...)
+			break loop
+		case node1 == nil && node2 != nil:
+			digits = append(digits, minTime(node2)...)
+			break loop
+		}
+
+		sorted := sortedKeySet(node1.Children, node2.Children)
+
+		// find the smallest digit which hash diff
+		foundDiff := false
+
+		for _, d := range sorted {
+			c1, c2 := node1.Children[d], node2.Children[d]
+
+			if (c1 != nil && c2 == nil) ||
+				(c1 == nil && c2 != nil) ||
+				(c1 != nil && c2 != nil && c1.Hash != c2.Hash) {
+				digits = append(digits, d)
+				node1 = c1
+				node2 = c2
+				foundDiff = true
+				break
+			}
+		}
+
+		if !foundDiff {
+			break loop
+		}
+	}
+
+	if len(digits) == 0 {
+		return time.Time{}
+	}
+
+	nano := digitsToNum(digits) * SlotMilli * int64(time.Millisecond)
+	return time.Unix(0, nano)
+}
+
+func numToDigits(num int64) []int64 {
+	digitCount := int(math.Log10(float64(num))) + 1
+	res := make([]int64, 0, digitCount)
+
+	for i := digitCount - 1; i >= 0; i-- {
+		digit := int64(math.Mod(float64(num)/math.Pow(10, float64(i)), 10))
+		res = append(res, digit)
+	}
+
+	return res
+}
+
+func digitsToNum(digits []uint8) int64 {
+	var res = int64(0)
+	var n = len(digits)
+
+	for i := 0; i < n; i++ {
+		res += int64(digits[i]) * int64(math.Pow10(n-i-1))
+	}
+
+	return res
 }
 
 func sortedKeySet(t1 map[uint8]*Trie, t2 map[uint8]*Trie) []uint8 {
@@ -130,54 +181,4 @@ func minTime(trie *Trie) []uint8 {
 	}
 
 	return keys
-}
-
-func BranchPoint(trie1 *Trie, trie2 *Trie) time.Time {
-	node1 := trie1
-	node2 := trie2
-	digits := []uint8{}
-
-loop:
-	for {
-		switch {
-		case node1 == nil && node2 == nil:
-			break loop
-		case node1 != nil && node2 == nil:
-			digits = append(digits, minTime(node1)...)
-			break loop
-		case node1 == nil && node2 != nil:
-			digits = append(digits, minTime(node2)...)
-			break loop
-		}
-
-		sorted := sortedKeySet(node1.Children, node2.Children)
-
-		// find the smallest digit which hash diff
-		foundDiff := false
-
-		for _, d := range sorted {
-			c1, c2 := node1.Children[d], node2.Children[d]
-
-			if (c1 != nil && c2 == nil) ||
-				(c1 == nil && c2 != nil) ||
-				(c1 != nil && c2 != nil && c1.Hash != c2.Hash) {
-				digits = append(digits, d)
-				node1 = c1
-				node2 = c2
-				foundDiff = true
-				break
-			}
-		}
-
-		if !foundDiff {
-			break loop
-		}
-	}
-
-	if len(digits) == 0 {
-		return time.Time{}
-	}
-
-	nano := digitsToNum(digits) * SlotMilli * int64(time.Millisecond)
-	return time.Unix(0, nano)
 }
